c6fb6f9e9746846e19c1460dec9c3e2ff2420425
Zarant <26726709+Zarant@users.noreply.github.com>
2021-10-09 14:31:05 -0300

    bug fix

--- _Lvl1QuickSetup.lua
+++ _Lvl1QuickSetup.lua
@@ -1,4 +1,4 @@
-l addonName,addon = ...
+local addonName,addon = ...
 local _, class = UnitClass("player");
 local _,race = UnitRace("player")
 local Frame = CreateFrame("Frame");
@@ -366,3 +366,17 @@ hooksecurefunc("UseAction",function(...)
 		StartHSTimer()
 	end
 end)
+
+
+
+function rf1()
+--LoadAddOn("Blizzard_CompactRaidFrames")
+
+--function sp(f,i) tr="TOPRIGHT";f2=f.debuffFrames;s=f2[1]:GetWidth();f3=f2[i];f3:SetSize(s,s);f3:ClearAllPoints();if i>6 then f3:SetPoint("BOTTOMRIGHT",f2[i-3],tr,0,0) else f3:SetPoint(tr,f2[1],tr,-(s*(i-3)),0) end end
+
+--function CBF(f,i) bf=CreateFrame("Button",f:GetName().."Debuff"..i,f,"CompactDebuffTemplate");bf.baseSize=22;bf:SetSize(f.buffFrames[1]:GetSize()) end;function mv(f) for i=4,12 do sp(f,i) end end
+
+--function mv3(f) CompactUnitFrame_SetMaxDebuffs(f,12); if not f.debuffFrames[4] then for i=4,12 do CBF(f,i) end end mv(f) end;hooksecurefunc("CompactUnitFrame_UpdateDebuffs",function(f) if f:GetName():match("^Compact") then mv3(f) end end);
+CRFSort_Group=function(t1, t2) if UnitIsUnit(t1,"player") then return false elseif UnitIsUnit(t2,"player") then return true else return t1 < t2 end end CompactRaidFrameContainer.flowSortFunc=CRFSort_Group
+
+end
